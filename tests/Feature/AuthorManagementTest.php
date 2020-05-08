ae543347bf16b702e53e4b60e18b30f75ae77529
Igor F <ig.freid@gmail.com>
2020-05-08 23:15:05 +0600

    basic tests for Author

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use \App\Author;
+
+class AuthorManagementTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+            'name' => 'Author Name',
+            'dob' => '04/03/1983'
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('1983/03/04', $author->first()->dob->format('Y/d/m'));
+    }
+}

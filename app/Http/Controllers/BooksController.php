5129c19de67154c76d2e00c663519c49ec78d042
Igor F <ig.freid@gmail.com>
2020-05-08 23:14:31 +0600

    wp for book resource

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -7,14 +7,37 @@ use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
+    /**
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function Store()
     {
         $book = Book::create($this->validateRequest());
+
+        return redirect($book->path());
     }
 
+    /**
+     * @param Book $book
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function update(Book $book)
     {
         $book->update($this->validateRequest());
+
+        return redirect($book->path());
+    }
+
+    /**
+     * @param Book $book
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @throws \Exception
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return redirect('/books');
     }
 
     /**
--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -7,22 +7,22 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use \App\Book;
 
-class BookReservationTest extends TestCase
+class BookManagementTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
     public function a_book_can_be_added_to_the_library()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->post('/books', [
             'title' => 'Cool Book',
             'author' => 'Vasya'
         ]);
 
-        $response->assertOk();
+        $book = Book::first();
+
         $this->assertCount(1, Book::all());
+        $response->assertRedirect($book->path());
     }
 
     /** @test */
@@ -50,8 +50,6 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
-
         $this->post('/books', [
             'title' => 'Cool Book',
             'author' => 'Vasya'
@@ -59,14 +57,31 @@ class BookReservationTest extends TestCase
 
         $book = Book::first();
 
-        $response = $this->patch("/books/{$book->id}", [
+        $response = $this->patch($book->path(), [
             'title' => 'New Title',
             'author' => 'New Author'
         ]);
 
-        $this->assertTrue(true);
+        $this->assertEquals('New Title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
+        $response->assertRedirect($book->fresh()->path());
+    }
 
-        //$this->assertEquals('New Title', Book::first()->title);
-        //$this->assertEquals('New Author', Book::first()->author);
+    /** @test */
+    public function a_book_can_be_deleted()
+    {
+        $this->post('/books', [
+            'title' => 'Cool Book',
+            'author' => 'Vasya'
+        ]);
+
+        $book = Book::first();
+
+        $this->assertCount(1, Book::all());
+
+        $response = $this->delete($book->path());
+
+        $this->assertCount(0, Book::all());
+        $response->assertRedirect('/books');
     }
 }

4592c61b87a26926e37871e0c798d5fc39d3b9af
Igor F <ig.freid@gmail.com>
2020-05-08 23:12:21 +0600

    new path to book resource added

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $fillable = ['title', 'author'];
+
+    /**
+     * @return string
+     */
+    public function path()
+    {
+        return "/books/{$this->id}";
+    }
 }

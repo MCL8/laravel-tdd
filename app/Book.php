e121ab94e0ee68b0e4bf40086dda1ff53773c60c
Igor F <ig.freid@gmail.com>
2020-05-18 02:15:23 +0600

    book checkout feature test

--- app/Book.php
+++ app/Book.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use App\Reservation;
 
 class Book extends Model
 {
@@ -17,10 +18,52 @@ class Book extends Model
         return "/books/{$this->id}";
     }
 
+    /**
+     * @param $user
+     */
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now()
+        ]);
+    }
+
+    /**
+     * @param $user
+     * @throws \Exception
+     */
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+    }
+
+    /**
+     * @param $author
+     */
     public function setAuthorIdAttribute($author)
     {
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author,
         ]))->id;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
